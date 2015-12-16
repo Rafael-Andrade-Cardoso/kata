fc058d90ff358cd9364188c13dc46ca8cc9fbd6d
rafael <rafael.pg@outlook.com>
2015-12-16 13:22:38 -0200

    Validação de CPF aluno e instrutor

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -23,6 +23,8 @@
       <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
       <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
+    <script src="<?php echo base_url('assets/js/valida_cpf_cnpj.js');?>"></script>
     <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+      
   </head>
 
